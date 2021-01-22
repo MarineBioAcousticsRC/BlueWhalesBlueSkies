f0ad93e966bb59185c0fb4bd75aa322f02b44eca
Vanessa ZoBell <53057768+vzobell@users.noreply.github.com>
2021-01-22 13:16:37 -0800

    Spherical Spreading TL Frequency Dependent
    This is a function that calculates frequency dependent spherical spreading transmission loss between a source and a receiver. The function inputs are the start and end frequency, depth of receiver, depth of source, horizontal range from source to receiver, pH, temperature, salinity, and sound speed. The function output is the frequency dependent spherical spreading transmission loss for the frequencies under investigation.

---
+++ PropagationModels/range2SS_TL.m
@@ -0,0 +1,52 @@
+function [TL_SS] = range2SS_TL(f0, f1, zr, zs, r, pH, T, S, c)
+
+% take Range, Depth, Frequency, Salinity, Temperature, and pH to
+% compute the frequency dependent spherical spreading between a source and
+% a receiver
+%
+% f0 = start frequency (Hz)
+% f1 = end frequency (Hz)
+% zr = depth of receiver (m)
+% zs = depth of source (m)
+% r = range to receiver (m)
+% pH = pH
+% T = temperature
+% S = salinity
+% c = sound speed (m/s)
+
+f = f0:f1; %range of frequencies
+f = f/1000; %changing it to kHz
+f = f';
+D = zr - zs; % vertical depth from source to receiver
+
+slantRange = sqrt((r.^2) + D.^2); %slant range from receiver to source
+
+%Boric Acid Contribution
+A1 = (8.86/c)*10^((0.78*pH) -5);
+P1 = 1;
+F1 = (2.8*((S/35)^0.5))*10^(4-(1245/(273+T)));
+
+%MgSO4 contribution
+A2 = 21.44*(S/c)*(1+0.025*T);
+P2 = 1-((1.37*(10^-4))*D) + (6.2*(10^-9))*D^2;
+F2 = (8.17*10^(8-1990/(273+T)))/(1+0.0018*(S-35));
+
+%Pure Water Contribution
+A3 = (4.937*10^-4) - (2.59*10^-5)*T + (9.11*(10^-7))*T^2 - (1.50*(10^-8))*T^3;
+P3 = 1-(3.83*(10^-5))*D + (4.9*(10^-10))*D^2;
+
+
+%alpha equation
+%total absorption = boric acid contrib. + MgSO4 contrib. + P. Water contrib.
+for n = 1:length(f)
+    alpha(n) = (A1*P1*F1*(f(n)^2))/(f(n)^2 + F1^2) + (A2*P2*F2*(f(n)^2))/((f(n)^2)+F2^2) +A3*P3*(f(n)^2);
+end
+%%%the units for alpha are dB/km, need to get it in meters like our slant
+%%%range
+alpha = alpha/1000;
+alpha = alpha';
+TL = 20*log10(slantRange);%spherical spreading transmission loss at each frequency
+
+%adding alpha to the spherical spreading model for frequency dependent TL
+TL_SS = TL + alpha*(slantRange);
+end
